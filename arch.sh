4743f30be92633862f440a5a0eb8e8a88208b996
Ajax <ajax@Ajax.Ajax>
2020-11-14 19:44:08 -0300

    condicoes para users

--- arch.sh
+++ arch.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
+#CONFIGURACAO DE TEMPO
 time=`ln -sf /usr/share/zoneinfo/America/Recife /etc/localtime; hwclock --systohc`
 
+#CONFIGURACAO DE LINGUA
 lang_pt=`echo LANG=pt_BR.UTF-8 >> /etc/locale.conf`
 
-
+#CONFIFURAÇÃO DE TECLADO
 keymap=`echo KEYMAP=br-abnt2 >> /etc/vconsole.conf`
 
+#HOSTNAME
 echo -e "\nInforme um nome ao hostname:"
  read name_host
   hostname=`echo $name_host >> /etc/hostname`
@@ -13,6 +17,7 @@ echo -e "\nInforme um nome ao hostname:"
     hosts2=`echo ::1 localhost.localdomain localhost >> /etc/hosts`
      host3=`echo 112.0.0.1 $name_host.localdomain $name_host >> /etc/hosts`
 
+#GRUB
 echo -e "Instalar o Grub?"
  grub=`pacman -S grub os-prober`
 
@@ -20,14 +25,21 @@ echo "Informe a Partição de Instalação da Grub"
  read disc
   grub_install=`grub-install $disc; grub-mkconfig -o /boot/grub/grub.cfg`
 
-
+#SENHA
 echo -e "\n\nInisira uma nova senha ao Root:\n"
  password=`passwd`
 
-echo -e "Instalação de ferramentas de rede"
+#REDE
+echo -e "\nInstalação de ferramentas de rede"
  rede=`pacman -S dhcpcd`
- startRede=`systemctl enable dhcpcd`
 
+if `systemctl enable dhcpcd`
+  then
+     echo -e "REDE ATIVA ;)\n"
+else
+    echo -e "REDE NAO ATIVA :( \n"
+fi
+#NOVO USUARIO
 echo -e "\nDeseja criar um novo usuario?(ESCREVA true ou false)\n"
 read condition
 
@@ -44,9 +56,11 @@ if $condition;
   
  echo -e "INSTALAR SUDO"
   sudo=`pacman -S sudo`
-  sudoers=`echo $name_user ALL=(ALL) ALL >> /etc/sudoers`
+  sudo_user="$name_user ALL=(ALL) ALL"
+  sudoers=`echo $sudo_user >> /etc/sudoers`
 
 fi
-echo -e '\n\nCONFIGURAÇÃO FINALIZADA!!!\n'
+
+echo -e '\n\nCONFIGURACAO FINALIZADA!!!\n'
 echo "Bye ;)"
 
